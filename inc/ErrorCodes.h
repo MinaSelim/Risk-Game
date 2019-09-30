8e2b55166403a5c8ea5d14ebf58348d0fd1d8406
Patrick <33182833+patrickl13@users.noreply.github.com>
2019-09-30 14:40:00 -0400

    Task3 dice (#7)
    * Fixed debugger not showing output, added Dice class
    
    * removed Dice.h and Dice.cpp in root directory
    
    * removed unecessary for loop
    
    * fixed issues, added DiceDriver, Dice.cpp/.h has 2 classes now
    
    * pointers, rollHistory breakdown, fixes from previous PR

--- inc/ErrorCodes.h
+++ inc/ErrorCodes.h
@@ -5,3 +5,4 @@
 #define OKAY 0;
 #define ERROR 1;
 #define INVALID_MAP 2;
+#define INVALID_DICEROLL 3;
---
+++ src/DiceDriver.cpp
@@ -0,0 +1,41 @@
+#include "Dice.h"
+#include "DiceDriver.h"
+
+void DiceDriver::DiceTester(){
+
+	DicesRoller * diceRoller1 = new DicesRoller();
+	DicesRoller * diceRoller2 = new DicesRoller();
+
+	DicesPrompt * dicesPrompt = new DicesPrompt();
+
+	int numDice = dicesPrompt->getRolledNumberOfDice();
+	diceRoller1->roll(numDice);
+	diceRoller1->showRollHistory();
+
+	numDice = dicesPrompt->getRolledNumberOfDice();
+	diceRoller2->roll(numDice);
+	diceRoller2->showRollHistory();
+
+	delete diceRoller1;
+	delete diceRoller2;
+	delete dicesPrompt;
+
+
+	/*Testing the "fairness" of the Dice
+	//we will roll the Dice many times and check the percentage of each roll
+	Assuming "fair" dice will be more or less split evenly between the face values of the dice [1-6]*/
+
+
+	//DicesRoller * diceRoller3 = new DicesRoller;
+	//int numDice = 3; //we will ignore the prompt because we are rolling many times, setting the dice to 3 will provide us more data
+
+	//for (unsigned int i = 0; i < 1000; i++) 
+	//{
+	//	diceRoller3->roll(numDice);
+	//}
+
+	//diceRoller3->showRollHistory();
+	//
+	//delete diceRoller3;
+
+}
