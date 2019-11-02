10a7e9a456d31acbcd1395ad467afe85f5eee75f
Mina Selim <supermina55555@hotmail.com>
2019-11-02 16:03:18 -0400

    A2/task 1 2 3 (#14)
    * Task 1 complete
    
    * Task 3 complete, Task 2 is semi-complete (need the reinforce method)

--- src/PlayerDriver.cpp
+++ src/PlayerDriver.cpp
@@ -6,7 +6,7 @@
 
 void PlayerDriver::PlayerTester()
 {
-	Map * map = MapLoader::loadMap("./worldNew.map");
+	/*	Map * map = MapLoader::loadMap("./maps/worldNew.map");
 	auto mapNode = map->getFirstNode();
 	std::vector<CountryNode*> countries;
 	std::vector<CountryNode*> * kok = new std::vector< CountryNode*>();
@@ -24,54 +24,8 @@ void PlayerDriver::PlayerTester()
 	Deck* deck = new Deck(map->getNumberOfCountriesInMap());*/
 
 	//Testing the functionality of the three methods:
-	player->attack();
-	player->fortify();
-	player->reinforce();
+	//player->attack();
+	//player->fortify();
+	//player->reinforce();
 
-	//
-	//cout << "The deck has " << deck->getSize() << " " << " cards" << endl;
-	//player->getHandOfCards()->pickACard(deck);
-	//player->getHandOfCards()->pickACard(deck);
-	//player->getHandOfCards()->pickACard(deck);
-	//player->getHandOfCards()->pickACard(deck);
-	//player->getHandOfCards()->pickACard(deck);
-	//cout << "The deck is left with " << deck->getSize() << " " << " cards" << endl;
-
-
-	//cout << "The player has no w in their hand: " << endl;
-	//player->getHandOfCards()->PrintValues();
-
-	//int numberOfCardsWillBeGiven = 0;
-	//if (player->getHandOfCards()->getTotalNumberOfCards() >= 5)
-	//{
-	//	for (int i = 0; i < 2; i++)
-	//	{
-	//		numberOfCardsWillBeGiven = player->getHandOfCards()->exchange();
-	//		cout << "The number of given this round is " << numberOfCardsWillBeGiven << " cards" << endl;
-	//		player->setNumberOfArmies(player->getNumberOfArmies() + numberOfCardsWillBeGiven);
-	//		cout << "The number of the player's armies after executing exchange is: " << player->getNumberOfArmies() << endl;
-	//	}
-	//}
-	//else
-	//{
-	//	numberOfCardsWillBeGiven = player->getHandOfCards()->exchange();
-	//	cout << "The number of given this round is " << numberOfCardsWillBeGiven << " cards" << endl;
-	//	player->setNumberOfArmies(player->getNumberOfArmies() + numberOfCardsWillBeGiven);
-	//	cout << "The number of the player's armies after executing exchange is: " << player->getNumberOfArmies() << endl;
-	//}
-	//
-
-	////Player's Dice Test:
-	//int numDice = dicesPrompt->getRolledNumberOfDice();
-	//player->getDice()->roll(numDice);
-	//player->getDice()->showRollHistory();
-
-	//numDice = dicesPrompt->getRolledNumberOfDice();
-	//player->getDice()->roll(numDice);
-	//player->getDice()->showRollHistory();
-	//
-	//delete player;
-	//delete map;
-	//delete dicesPrompt;
-	//delete deck;
 }
